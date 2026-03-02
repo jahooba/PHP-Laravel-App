3921395d8713771cf8f850fc35a3a170f2e92f65
jahooba <josh.uapena20002@gmail.com>
2026-03-02 15:02:37 -0800

    Adjusted CSS styling *added div for list of cards for flexbox

--- resources/views/inspections/index.blade.php
+++ resources/views/inspections/index.blade.php
@@ -3,17 +3,19 @@
 @section('content')
     <h1>Inspection List</h1>
 
-    @foreach ($inspections as $inspection)
-        <div class={{ $inspection->passed ? "card-pass" : "card-fail" }}>
-            Vehicle: <strong>{{ $inspection->vehicle_name }}</strong> <br>
-            Emission: {{ $inspection->emission_level }} <br>
-            Status:
-            @if ($inspection->passed)
-                <span class="passed">Passed</span>
-            @else
-                <span class="failed">Failed</span>
+    <div class="card-list">
+        @foreach ($inspections as $inspection)
+            <div class={{ $inspection->passed ? "card-pass" : "card-fail" }}>
+                Vehicle: <strong>{{ $inspection->vehicle_name }}</strong> <br>
+                Emission: {{ $inspection->emission_level }} <br>
+                Status:
+                @if ($inspection->passed)
+                    <span class="passed">Passed</span>
+                @else
+                    <span class="failed">Failed</span>
 
-            @endif
-        </div>
-    @endforeach
+                @endif
+            </div>
+        @endforeach
+    </div>
 @endsection
