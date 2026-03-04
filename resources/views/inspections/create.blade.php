8b07df60dd483f331c07a8e3c9df73f945430a22
jahooba <josh.uapena20002@gmail.com>
2026-03-03 18:04:03 -0800

    Stylized page for new inspections *adjusted background color

--- resources/views/inspections/create.blade.php
+++ resources/views/inspections/create.blade.php
@@ -1,10 +1,27 @@
-<h>Add inspection</h>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Inspection App</title>
+        <link rel="stylesheet" href="{{ asset('css/inspection_app.css') }}">
+    </head>
 
-<form method="POST" action="/inspections">
-    @csrf
+    <body>
+        <nav>
+            <h1>Vehicle Inspection System</h1>
+            <a href="/inspections"> All Inspections</a>
+            <a href="/inspections/create"> New Inspection</a>
+        </nav>
 
-    <input type="text" name="vehicle_name" placeholder="Vehicle Name">
-    <input type="number" name="emission_level" placeholder="Emission Level">
+        <div class="container">
+            <h1>Add inspection</h1>
 
-    <button type="submit">Submit</button>
-</form>
+            <form method="POST" action="/inspections">
+                @csrf
+                <input type="text" name="vehicle_name" placeholder="Vehicle Name">
+                <input type="number" name="emission_level" placeholder="Emission Level">
+
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    </body>
+</html>
