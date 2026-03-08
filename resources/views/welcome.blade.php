1ab48e0bcb2e9f1921fbda43245b8193fc2fc317
jahooba <josh.uapena20002@gmail.com>
2026-03-07 22:17:11 -0800

    Fixed UI and CSS formatting *added vector icons for home page buttons

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,8 +6,27 @@
     </head>
 
     <body>
-        <div>
-            @include('inspections.nav_bar')
+        <div class="home-text">
+            <h1>View and submit vehicle inspections.</h1>
+            <h3>Automatically detects if vehicle passed or failed emission standards.</h3> <br>
+        </div>
+
+        <div class="hero">
+            <a href="/inspections" class="link-wrapper">
+                <div class="hero-all">
+                    <p><strong>All Inspections</strong></p>
+                    <p>View list of all submitted inspections.</p>
+                    <img src="images/list_icon.png" alt="list icon">
+                </div>
+            </a>
+
+            <a href="/inspections/create" class="link-wrapper">
+                <div class="hero-new">
+                    <p><strong>New Inspection</strong></p>
+                    <p>Create and submit a new inspection.</p>
+                    <img src="/images/pencil_icon.png" alt="pencil icon">
+                </div>
+            </a>
         </div>
     </body>
 </html>
